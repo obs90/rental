2773059511aef2c7e64766306c368ccecb0275dd
oubaid <obeysd12@gmail.com>
2026-02-05 19:57:04 +0100

    ajouter poids

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -75,7 +75,7 @@ export function HomePage({ onNavigateToEquipment }: HomePageProps) {
         </div>
       </section>
 
-      {/* Categories Sectionn */}
+      {/* Categories Section */}
       <section className="py-20 bg-gradient-to-b from-white to-gray-50">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="text-center mb-16">
